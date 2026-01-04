d0033e1a83366d2cbcfd74c2a2dae0f463f4e8a4
Master <>
2026-01-04 20:13:58 +0800

    update barchart

--- app.vue
+++ app.vue
@@ -8,3 +8,14 @@
     </ClientOnly>
   </div>
 </template>
+
+<!-- <script>
+import ApexCharts from "apexcharts";
+
+export default {
+  setup() {
+    window.ApexCharts = ApexCharts;
+  },
+  mounted() {},
+};
+</script> -->
